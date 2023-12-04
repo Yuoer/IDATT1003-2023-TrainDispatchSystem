078478fa1ac96daf452bc9eb4d465977c3ea1c9d
Yuoer <augustreitanbogseth@gmail.com>
2023-12-04 14:54:50 +0100

    Added javadoc comments and checks to verify valid parameters

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -2,6 +2,7 @@ package edu.ntnu.stud;
 
 import java.time.LocalTime;
 
+
 /**
  * This class represents a train departure.
  * A train departure has a departure time, a line, a train number,
@@ -17,8 +18,8 @@ import java.time.LocalTime;
  *
  *
  * @author 562289
- * @version 0.1
- * @since 0.2
+ * @version 0.3
+ * @since 0.1
  */
 
 public class TrainDeparture {
@@ -30,10 +31,94 @@ public class TrainDeparture {
   private LocalTime delay;
 
   /**
-   * Constructs a train departure with the specified departure time,
-   * line, train number, destination, track and delay.
+   * Verifies that the hour and minute are valid.
    *
-   * @param departureTime the departure time of the train
+   * @param hour the hour to verify
+   *
+   * @param minute the minute to verify
+   *
+   * @throws IllegalArgumentException if the hour is less than 0 or greater than 23
+   * or if the minute is less than 0 or greater than 59
+   */
+  public void verifyTime(int hour, int minute) throws IllegalArgumentException {
+    if (hour < 0 || hour > 23) {
+      throw new IllegalArgumentException("Hour must be between 0 and 23");
+    }
+    if (minute < 0 || minute > 59) {
+      throw new IllegalArgumentException("Minute must be between 0 and 59");
+    }
+  }
+
+  /**
+   * Verifies that the line is valid.
+   *
+   * @param line the line to verify
+   *
+   * @throws IllegalArgumentException if the line is null
+   * or if the line is less than 2 or greater than 3 characters
+   * or if the line contains characters other than capital letters and numbers
+   */
+  public void verifyLine(String line) throws IllegalArgumentException {
+    if (line.length() < 2 || line.length() > 3) {
+      throw new IllegalArgumentException("Line must be between 2 and 3 characters");
+    }
+    if (!line.matches("[A-Z0-9]+")) {
+      throw new IllegalArgumentException("Line must contain only capital letters and numbers");
+    }
+  }
+
+  /**
+   * Verifies that the train number is valid.
+   *
+   * @param trainNumber the train number to verify
+   *
+   * @throws IllegalArgumentException if the train number is less than 0
+   */
+  public void verifyTrainNumber(int trainNumber) throws IllegalArgumentException {
+    if (trainNumber < 1 || trainNumber > 999) {
+      throw new IllegalArgumentException("Train number has to be between 1 and 999");
+    }
+  }
+
+  /**
+   * Verifies that the destination is valid.
+   *
+   * @param destination the destination to verify
+   *
+   * @throws IllegalArgumentException if the destination is null
+   * or if the destination is less than 2 or greater than 30 characters
+   * or if the destination contains characters other than letters
+   */
+  public void verifyDestination(String destination) throws IllegalArgumentException {
+    if (destination.length() < 2 || destination.length() > 30) {
+      throw new IllegalArgumentException("Destination must be between 2 and 30 characters");
+    }
+    if (!destination.matches("[a-zA-ZæøåÆØÅ]+")) {
+      throw new IllegalArgumentException("Destination must contain only letters");
+    }
+  }
+
+  /**
+   * Verifies that the track is valid.
+   *
+   * @param track the track to verify
+   *
+   * @throws IllegalArgumentException if the track is less than 1 or greater than 99
+   */
+  public void verifyTrack(int track) throws IllegalArgumentException {
+    if (track != -1 && track < 1 || track > 99) {
+      throw new IllegalArgumentException("Track must be between 1 and 99 or -1");
+    }
+  }
+
+  /**
+   * Constructs a train departure with the specified departure time,
+   * line, train number, destination, track and delay. Checks that the
+   * departure time, line, train number, destination, track and delay are valid.
+   *
+   * @param departureHour the departure hour of the train
+   *
+   * @param departureMinute the departure minute of the train
    *
    * @param line the line of the train
    *
@@ -43,16 +128,26 @@ public class TrainDeparture {
    *
    * @param track the track of the train
    *
-   * @param delay the delay of the train
+   * @param delayHour the hour delay of the train
+   *
+   * @param delayMinute the minute delay of the train
    */
-  public TrainDeparture(LocalTime departureTime, String line, int trainNumber,
-                        String destination, int track, LocalTime delay) {
-    this.departureTime = departureTime;
+  public TrainDeparture(int departureHour, int departureMinute, String line, int trainNumber,
+                        String destination, int track, int delayHour, int delayMinute)
+      throws IllegalArgumentException {
+    verifyTime(departureHour, departureMinute);
+    verifyLine(line);
+    verifyTrainNumber(trainNumber);
+    verifyDestination(destination);
+    verifyTrack(track);
+    verifyTime(delayHour, delayMinute);
+
+    this.departureTime = LocalTime.of(departureHour, departureMinute);
     this.line = line;
     this.trainNumber = trainNumber;
     this.destination = destination;
     this.track = track;
-    this.delay = delay;
+    this.delay = LocalTime.of(delayHour, delayMinute);
   }
 
   /**
@@ -114,7 +209,8 @@ public class TrainDeparture {
    *
    * @param line the line of the train
    */
-  public void setLine(String line) {
+  public void setLine(String line) throws IllegalArgumentException {
+    verifyLine(line);
     this.line = line;
   }
 
@@ -123,17 +219,19 @@ public class TrainDeparture {
    *
    * @param track the track of the train
    */
-  public void setTrack(int track) {
+  public void setTrack(int track) throws IllegalArgumentException {
+    verifyTrack(track);
     this.track = track;
   }
 
   /**
    * Sets the delay of the train departure.
    *
-   * @param delay the delay of the train
+   * @param delayHour the delay of the train
    */
-  public void setDelay(LocalTime delay) {
-    this.delay = delay;
+  public void setDelay(int delayHour, int delayMinute) throws IllegalArgumentException {
+    verifyTime(delayHour, delayMinute);
+    this.delay = LocalTime.of(delayHour, delayMinute);
   }
 
   /**
