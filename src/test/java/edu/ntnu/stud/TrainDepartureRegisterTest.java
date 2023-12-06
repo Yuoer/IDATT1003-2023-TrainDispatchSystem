df45abd3daaf10537c67018c552edd358db703ac
Yuoer <augustreitanbogseth@gmail.com>
2023-12-06 12:09:59 +0100

    Created TrainDepartureRegisterTest

---
+++ src/test/java/edu/ntnu/stud/TrainDepartureRegisterTest.java
@@ -0,0 +1,13 @@
+package edu.ntnu.stud;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+public class TrainDepartureRegisterTest {
+  
+}
